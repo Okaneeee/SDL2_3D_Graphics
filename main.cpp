b925a4a497c4517ab10d32fe2f7741f1e7a22a23
Anatole <anatole.giniaux@sfr.fr>
2024-11-16 23:48:56 +0100

    feat: initial commit
    CMakeLists and a dark window that closes automatically after 3 secondes

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#define SDL_MAIN_HANDLED // Avoid WinMain error
+#include <SDL.h>
+
+int main(int argc, char* argv[]) {
+    // SDL initialization
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Erreur d'initialisation de SDL : %s", SDL_GetError());
+        return -1;
+    }
+
+    // SDL Window
+    SDL_Window* window = SDL_CreateWindow("Test",
+                                          SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                          800, 600, SDL_WINDOW_SHOWN);
+    if (!window) {
+        SDL_Log("Error when creating the window : %s", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    // Wait 3 seconds
+    SDL_Delay(3000);
+
+    // Clean and quit
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
